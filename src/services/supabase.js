05dcdb1e946bc21c059781c6c7df8392a955d9d6
zbtechph <zbtech@1fidel.tech>
2021-10-09 20:14:17 +0800

    throw error if supabase key or url is empty

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,4 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 const url = import.meta.env.VITE_SUPABASE_URL;
 const anonkey = import.meta.env.VITE_SUPABASE_PUBLIC_KEY;
 
+if(!url || !anonkey) throw new Error("Supabase credentials is missing")
+
 export default createClient(url, anonkey)
